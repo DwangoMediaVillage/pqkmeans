11d78938a83cfb1466dd91d26b48893118fba97c
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-07-26 01:35:17 +0900

    :bug: fix invalid type conversion

--- src/encoder/encoder_sample.cpp
+++ src/encoder/encoder_sample.cpp
@@ -4,7 +4,7 @@ void EncoderSample::fit_generator(boost::python::object iterator) {
     boost::python::stl_input_iterator<boost::python::list> begin(iterator), end;
     for (auto itr = begin; itr != end; ++itr) {
         auto item = *itr;
-        std::vector<boost::python::object>::iterator found = std::find(index_dict.begin(), index_dict.end(), item);
+        auto found = std::find(index_dict.begin(), index_dict.end(), item);
         if (found == index_dict.end()) {
             index_dict.push_back(item);
         }
@@ -13,7 +13,7 @@ void EncoderSample::fit_generator(boost::python::object iterator) {
 
 
 boost::python::list EncoderSample::transform_one(boost::python::list vector) {
-    std::vector<boost::python::list>::iterator found = std::find(index_dict.begin(), index_dict.end(), vector);
+    auto found = std::find(index_dict.begin(), index_dict.end(), vector);
     if (found != index_dict.end()) {
         boost::python::list ret_vector;
         ret_vector.append(std::distance(index_dict.begin(), found));
@@ -25,7 +25,6 @@ boost::python::list EncoderSample::transform_one(boost::python::list vector) {
 
 boost::python::list EncoderSample::inverse_transform_one(boost::python::list vector) {
     long value = boost::python::extract<long>(vector[0]);
-    std::vector<boost::python::object>::iterator found = std::find(index_dict.begin(), index_dict.end(), vector);
     if (value < index_dict.size()) {
         return index_dict[value];
     } else {
