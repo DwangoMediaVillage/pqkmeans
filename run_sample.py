65da0aea6ae3f6017ab1864c6c5386867a860254
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-07-28 20:44:43 +0900

    :art: inifinite sample

--- run_sample.py
+++ run_sample.py
@@ -1,16 +1,23 @@
 import pqkmeans
 
 
-def data_source():
-    return ([float(i*100)] for i in range(10) for _ in range(3))
+def data_source(n: int):
+    for i in range(n):
+        for _ in range(3):
+            yield [i * 100]
 
 
 e = pqkmeans.encoder.EncoderSample()
-e.fit_generator(data_source())
+e.fit_generator(data_source(20))
 
-for original, encoded, decoded in zip(
-        data_source(),
-        e.transform_generator(data_source()),
-        e.inverse_transform_generator(e.transform_generator(data_source()))
+# can handle infinite list
+inf = 1000000000
+for i, original, encoded, decoded in zip(
+        range(inf),
+        data_source(inf),
+        e.transform_generator(data_source(inf)),
+        e.inverse_transform_generator(e.transform_generator(data_source(inf)))
 ):
     print("{} -> {} -> {}".format(original, encoded, decoded))
+    if i == 10:
+        break
