15d2238ff7fe74b16cfe0b9c490a76ff02952f4d
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-07-28 16:34:37 +0900

    :star: setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,91 @@
-from setuptools import setup, find_packages
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+class CMakeExtension(Extension):
+    """
+    @see https://github.com/pybind/cmake_example/blob/master/setup.py
+    """
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    """
+    @see https://github.com/pybind/cmake_example/blob/master/setup.py
+    """
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2 ** 32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+# setup(
+#     name='pqkmeans',
+#     version='0.0.1',
+#     description='PQk-means',
+#     author='Yusuke MATSUI, Keisuke OGAKI',
+#     author_email='matsui@nii.ac.jp, keisuke_ogaki@dwango.co.jp',
+#     url='',
+#     packages=find_packages('.'),
+#     data_files=[(".", ["_pqkmeans.so"])],
+#     include_package_data=True,
+#     install_requires=[],
+#     test_suite='tests',
+#     entry_points='',
+#     zip_safe=False
+# )
 
 setup(
     name='pqkmeans',
     version='0.0.1',
-    description='PQk-means',
-    author='Yusuke MATSUI, Keisuke OGAKI',
-    author_email='matsui@nii.ac.jp, keisuke_ogaki@dwango.co.jp',
-    url='',
-    packages=find_packages('.'),
-    data_files=[(".", ["_pqkmeans.so"])],
-    include_package_data=True,
-    install_requires=[],
-    test_suite='tests',
-    entry_points='',
-    zip_safe=False
+    author='',
+    author_email='',
+    description='',
+    long_description='',
+    # packages=find_packages('.'),
+    ext_modules=[CMakeExtension('_pqkmeans')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
 )
