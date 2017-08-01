379fa3eff0ec3545129bc89854d9f162ab8668df
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-08-01 18:04:47 +0900

    :art: fix typo

--- src/_pqkmeans.cpp
+++ src/_pqkmeans.cpp
@@ -5,7 +5,7 @@
 namespace py = pybind11;
 
 PYBIND11_PLUGIN(_pqkmeans) {
-    py::module m("_pqkemasn", "internal module for PQk-means");
+    py::module m("_pqkemeans", "internal module for PQk-means");
 
     py::class_<EncoderSample>(m, "EncoderSample")
             .def(py::init<>())
@@ -14,11 +14,3 @@ PYBIND11_PLUGIN(_pqkmeans) {
             .def("inverse_transform_one", &EncoderSample::inverse_transform_one);
     return m.ptr();
 }
-
-//BOOST_PYTHON_MODULE (_pqkmeans) {
-//    using namespace boost::python;
-//    class_<EncoderSample>("EncoderSample")
-//            .def("fit_generator", &EncoderSample::fit_generator)
-//            .def("transform_one", &EncoderSample::transform_one)
-//            .def("inverse_transform_one", &EncoderSample::inverse_transform_one);
-//}
