4c4d16684326102abc6c4f5139ca3773b25befce
Yusuke Matsui <matsui@nii.ac.jp>
2017-09-12 18:20:41 +0900

    :bug:

--- pqkmeans/clustering/pqkmeans.py
+++ pqkmeans/clustering/pqkmeans.py
@@ -11,18 +11,15 @@ class PQKMeans(sklearn.base.BaseEstimator, sklearn.base.ClusterMixin):
         self._impl = _pqkmeans.PQKMeans(encoder.codewords, k, iteration, verbose)
 
     def predict_generator(self, x_test: typing.Iterable[typing.Iterable[numpy.uint8]]):
-        print("!!!!!!!!predict_generator")
         for vec in x_test:
             yield self._impl.predict_one(vec)
 
     def fit(self, x_train: numpy.array):
-        print("!!!!!!!!fit")
         assert len(x_train.shape) == 2
         self._impl.fit(x_train)
 
     def predict(self, x_test: numpy.array):
         assert len(x_test.shape) == 2
-        print("!!!!!!!!!!predict")
         return numpy.array(list(self.predict_generator(x_test)))
 
     @property
