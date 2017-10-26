82ec069f49f04eab5b092878d43bb5336f4f4a0c
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-10-26 22:49:18 +0900

    :star: v1.0.1

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pqkmeans',
-    version='1.0.0',
+    version='1.0.1',
     author='Keisuke Ogaki, Yusuke Matsui',
     author_email='keisuke_ogaki@dwango.co.jp, matsui528@gmail.com',
     license='MIT License',
