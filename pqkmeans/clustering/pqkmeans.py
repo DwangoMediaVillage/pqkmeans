8d8303b723d3aeed52365a4b4489e28148b4247b
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-12-29 00:49:36 +0900

    :art: use namedtuple for save

--- pqkmeans/clustering/pqkmeans.py
+++ pqkmeans/clustering/pqkmeans.py
@@ -1,8 +1,11 @@
 import typing
 import numpy
 import sklearn
-
+import collections
 import _pqkmeans
+
+PQKMeansSavedata = collections.namedtuple('PQKMeansSavedata',
+                                          ('encoder', 'k', 'iteration', 'verbose', 'cluster_centers'))
 
 
 class PQKMeans(sklearn.base.BaseEstimator, sklearn.base.ClusterMixin):
@@ -29,18 +32,19 @@ class PQKMeans(sklearn.base.BaseEstimator, sklearn.base.ClusterMixin):
         return numpy.array(list(self.predict_generator(x_test)))
 
     def __getstate__(self):
-        return {
-            "encoder": self.encoder,
-            "k": self._impl.k_,
-            "iteration": self._impl.iteration_,
-            "verbose": self._impl.verbose_,
-            "cluster_centers": self.cluster_centers_
-        }
+        return PQKMeansSavedata(
+            encoder=self.encoder,
+            k=self._impl.k_,
+            iteration=self._impl.k_,
+            verbose=self._impl.verbose_,
+            cluster_centers=self.cluster_centers_
+        )
 
     def __setstate__(self, state):
-        self._impl = _pqkmeans.PQKMeans(state["encoder"].codewords, state["k"], state["iteration"], state["verbose"],
+        # type: (PQKMeansSavedata) -> None
+        self._impl = _pqkmeans.PQKMeans(state.encoder.codewords, state.k, state.iteration, state.verbose,
                                         [])
-        self._impl.set_cluster_centers(state["cluster_centers"])
+        self._impl.set_cluster_centers(state.cluster_centers)
 
     @property
     def labels_(self):
